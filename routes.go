c436aed7905b4950cce1fe5b8bba9b34b1336c99
Gaurav Shukla <gaurav@gauravshukla.xyz>
2016-12-01 04:57:24 +0530

    added patch to preflight

--- routes.go
+++ routes.go
@@ -32,7 +32,7 @@ func withCORS(fn http.HandlerFunc) http.HandlerFunc {
  return func(w http.ResponseWriter, r *http.Request) {
    w.Header().Set("Access-Control-Allow-Origin", "*")
    w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PATCH, DELETE")
    fn(w, r)
  }
 }
